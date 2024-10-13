d14a38aaa819556c4bc58b8872e30e6bcb830398
andrafecske <andrafecske@gmail.com>
2024-10-13 11:48:08 +0300

    Aufgabe1

---
+++ src/Grades.java
@@ -0,0 +1,71 @@
+public class Grades {
+    public int[] grades;
+
+    public Grades(int[] grd) {
+        this.grades = grd;
+    }
+    //punkt1
+    public int[] notSufficient() {
+        int[] res = new int[grades.length];
+        int k = 0;
+        for (int grade : this.grades) {
+            if(grade<40)
+            {
+                res[k] = grade;
+                k++;
+            }
+        }
+
+        int[] newRes = new int[k];
+        for (int i = 0; i < newRes.length; i++) {
+            newRes[i] = res[i];
+        }
+
+
+        return newRes;
+    }
+
+    //punkt 2
+    public double averageGrade() {
+        double sum = 0.0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.length;  // length works for arrays
+    }
+
+    //punkt 3
+    public int[] rounded() {
+        int[] res = new int[grades.length];
+        int k = 0;
+        boolean valid;
+        for (int i = 0; i < grades.length; i++) {
+            int add=0;
+
+            for (int j = 1; j <= 3; j++) {
+                if(grades[i] < 38)
+                    break;
+
+                if ((grades[i] + j) % 5 == 0) {
+                    add =  j;
+                    break;
+                }
+            }
+            res[i] = grades[i] + add;
+
+        }
+        return res;
+    }
+
+    //punkt 4
+     public int maxGrade(){
+        int[] roundedGrades = this.rounded();
+        int max = -1;
+        for(int i = 0; i<roundedGrades.length; i++)
+        {
+            if(roundedGrades[i]>max)
+                max = roundedGrades[i];
+        }
+        return max;
+     }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] gradesArray = {29, 37, 38, 41, 84, 67};
+        Grades grade = new Grades(gradesArray);
+        int res[] = grade.notSufficient();
+
+        //punkt 1
+        for(int i = 0; i<res.length; i++)
+        {
+            System.out.print(res[i] + " ");
+        }
+
+        //punkt2
+        System.out.println();
+        double val = grade.averageGrade();
+        System.out.printf("%.2f", val);
+
+        //punkt3
+        System.out.println();
+        int res2[] = grade.rounded();
+        for(int i = 0; i<res2.length; i++)
+        {
+            System.out.print(res2[i] + " ");
+        }
+        System.out.println();
+
+        //punkt 4
+
+        System.out.println(grade.maxGrade());
     }
 }
